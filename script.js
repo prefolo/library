a78fd4ec99356676bbdcdebb5e76202cd3aedba7
“prefolo” <webintosh@yahoo.it>
2022-10-15 10:12:46 +0200

    Escape HTML in form entries

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function displayLibrary() {
         
         cardCode += `<div class="card"><button data-index="${i}" class="delete-btn"><svg viewBox="0 0 24 24">
 		<path fill="currentColor" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
-	</svg></button><div class="title">${book.title}</div><div class="book-properties-grid"><div class="property">Author</div><div>${ book.author }</div><div class="property">Pages</div><div>${book.pagesCount}</div><div class="property">Read</div><input data-index="${i}" class="read-btn" type="checkbox" ${ book.isRead ? 'checked' : '' }></div></div>`;
+	</svg></button><div class="title">${ escapeHTML(book.title) }</div><div class="book-properties-grid"><div class="property">Author</div><div>${ escapeHTML(book.author) }</div><div class="property">Pages</div><div>${ book.pagesCount }</div><div class="property">Read</div><input data-index="${i}" class="read-btn" type="checkbox" ${ book.isRead ? 'checked' : '' }></div></div>`;
     }
 
 	container.innerHTML = cardCode;
@@ -75,6 +75,19 @@ function toggleReadProp() {
     myLibrary[this.dataset.index].toggleReadProp();
     console.log(myLibrary)
 }
+
+const escapeHTML = str =>
+  str.replace(
+    /[&<>'"]/g,
+    tag =>
+      ({
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        "'": '&#39;',
+        '"': '&quot;'
+      }[tag] || tag)
+  );
 
 document.querySelector('#showFormForCreateNewBookBttn').addEventListener('click',showFormForCreateNewBook);
 document.querySelector('#saveBookBttn').addEventListener('click',saveBook);
