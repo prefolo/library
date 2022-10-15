27b3828a8ccc9260eac25fac224c4e714105404a
“prefolo” <webintosh@yahoo.it>
2022-10-15 09:20:39 +0200

    Implement book deletion

--- script.js
+++ script.js
@@ -22,13 +22,17 @@ function displayLibrary() {
     
     let cardCode = '';
 
-	for (const book of myLibrary) {
-		cardCode += `<div class="card"><button class="delete-btn"><svg viewBox="0 0 24 24">
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = myLibrary[i];
+        
+        cardCode += `<div class="card"><button data-index="${i}" class="delete-btn"><svg viewBox="0 0 24 24">
 		<path fill="currentColor" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
 	</svg></button><div class="title">${book.title}</div><div class="book-properties-grid"><div class="property">Author</div><div>${ book.author }</div><div class="property">Pages</div><div>${book.pagesCount}</div><div class="property">Read</div><input type="checkbox" ${ book.isRead ? 'checked' : '' }></div></div>`;
-	}
+    }
 
 	container.innerHTML = cardCode;
+
+    document.querySelectorAll(".delete-btn").forEach(btn => btn.addEventListener('click', deleteBook))
 }
 
 function showFormForCreateNewBook() {
@@ -57,6 +61,10 @@ function saveBook() {
 	cancelForm();
 }
 
+function deleteBook() {
+    myLibrary.splice(this.dataset.index, 1);
+    displayLibrary();
+}
 
 document.querySelector('#showFormForCreateNewBookBttn').addEventListener('click',showFormForCreateNewBook);
 document.querySelector('#saveBookBttn').addEventListener('click',saveBook);
