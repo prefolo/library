85e90f6b9c4b1d183972ef4952b002f70f775f20
“prefolo” <webintosh@yahoo.it>
2022-10-18 08:41:03 +0200

    Convert constructors to classes

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const myLibrary = [];
 
-function Book(title, author, pagesCount, isRead) {
-	this.title = title;
-	this.author = author;
-	this.pagesCount = pagesCount;
-	this.isRead = isRead;
+class Book {
+	constructor(title, author, pagesCount, isRead) {
+		this.title = title;
+		this.author = author;
+		this.pagesCount = pagesCount;
+		this.isRead = isRead;
+	}
 
-	this.info = function () {
+	info() {
 		return `${this.title} by ${this.author}, ${this.pagesCount} pages, ${
 			this.isRead ? 'read' : 'not read yet'
 		}`;
-	};
-}
+	}
 
-Book.prototype.toggleReadProp = function(){
-    this.isRead = !this.isRead;
+	toggleReadProp() {
+		this.isRead = !this.isRead;
+	}
 }
 
 function addBookToLibrary(title, author, pagesCount, isRead) {
@@ -23,30 +25,42 @@ function addBookToLibrary(title, author, pagesCount, isRead) {
 
 function displayLibrary() {
 	const container = document.querySelector('#cards-container');
-    
-    let cardCode = '';
 
-    for (let i = 0; i < myLibrary.length; i++) {
-        const book = myLibrary[i];
-        
-        cardCode += `<div class="card"><button data-index="${i}" class="delete-btn"><svg viewBox="0 0 24 24">
+	let cardCode = '';
+
+	for (let i = 0; i < myLibrary.length; i++) {
+		const book = myLibrary[i];
+
+		cardCode += `<div class="card"><button data-index="${i}" class="delete-btn"><svg viewBox="0 0 24 24">
 		<path fill="currentColor" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
-	</svg></button><div class="title">${ escapeHTML(book.title) }</div><div class="book-properties-grid"><div class="property">Author</div><div>${ escapeHTML(book.author) }</div><div class="property">Pages</div><div>${ book.pagesCount }</div><div class="property">Read</div><input data-index="${i}" class="read-btn" type="checkbox" ${ book.isRead ? 'checked' : '' }></div></div>`;
-    }
+	</svg></button><div class="title">${escapeHTML(
+		book.title
+	)}</div><div class="book-properties-grid"><div class="property">Author</div><div>${escapeHTML(
+			book.author
+		)}</div><div class="property">Pages</div><div>${
+			book.pagesCount
+		}</div><div class="property">Read</div><input data-index="${i}" class="read-btn" type="checkbox" ${
+			book.isRead ? 'checked' : ''
+		}></div></div>`;
+	}
 
 	container.innerHTML = cardCode;
 
-    document.querySelectorAll(".delete-btn").forEach(btn => btn.addEventListener('click', deleteBook));
-    document.querySelectorAll(".read-btn").forEach(btn => btn.addEventListener('click', toggleReadProp));
+	document
+		.querySelectorAll('.delete-btn')
+		.forEach((btn) => btn.addEventListener('click', deleteBook));
+	document
+		.querySelectorAll('.read-btn')
+		.forEach((btn) => btn.addEventListener('click', toggleReadProp));
 }
 
 function showFormForCreateNewBook() {
-	document.querySelector('#overlay').style.display = "grid";
+	document.querySelector('#overlay').style.display = 'grid';
 }
 
 function cancelForm() {
-	document.querySelector('#overlay').style.display = "none";
-	
+	document.querySelector('#overlay').style.display = 'none';
+
 	document.querySelector('#newbook-title').value = '';
 	document.querySelector('#newbook-author').value = '';
 	document.querySelector('#newbook-pages').value = 1;
@@ -59,7 +73,7 @@ function saveBook() {
 	const pages = document.querySelector('#newbook-pages').value;
 	const isRead = document.querySelector('#newbook-isRead').checked;
 
-	if( !( title && author ) ) return;
+	if (!(title && author)) return;
 
 	addBookToLibrary(title, author, pages, isRead);
 	displayLibrary();
@@ -67,35 +81,33 @@ function saveBook() {
 }
 
 function deleteBook() {
-    myLibrary.splice(this.dataset.index, 1);
-    displayLibrary();
+	myLibrary.splice(this.dataset.index, 1);
+	displayLibrary();
 }
 
 function toggleReadProp() {
-    myLibrary[this.dataset.index].toggleReadProp();
-    console.log(myLibrary)
+	myLibrary[this.dataset.index].toggleReadProp();
+	console.log(myLibrary);
 }
 
-const escapeHTML = str =>
-  str.replace(
-    /[&<>'"]/g,
-    tag =>
-      ({
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        "'": '&#39;',
-        '"': '&quot;'
-      }[tag] || tag)
-  );
+const escapeHTML = (str) =>
+	str.replace(
+		/[&<>'"]/g,
+		(tag) =>
+			({
+				'&': '&amp;',
+				'<': '&lt;',
+				'>': '&gt;',
+				"'": '&#39;',
+				'"': '&quot;',
+			}[tag] || tag)
+	);
 
-document.querySelector('#showFormForCreateNewBookBttn').addEventListener('click',showFormForCreateNewBook);
-document.querySelector('#saveBookBttn').addEventListener('click',saveBook);
-document.querySelector('#cancelForm').addEventListener('click',cancelForm);
-
-
-
-
+document
+	.querySelector('#showFormForCreateNewBookBttn')
+	.addEventListener('click', showFormForCreateNewBook);
+document.querySelector('#saveBookBttn').addEventListener('click', saveBook);
+document.querySelector('#cancelForm').addEventListener('click', cancelForm);
 
 addBookToLibrary('Odyssey', 'Homer', 344, false);
 addBookToLibrary('Divine Comedy', 'Dante Aligheri', 520, false);
